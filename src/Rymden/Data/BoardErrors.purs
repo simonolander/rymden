93008586596ec7422c937a7e14041936d656148e
Simon Olander <simon.olander@r2m.se>
2021-08-08 12:26:15 +0200

    Implement check for incorrect galaxy sizes

--- src/Rymden/Data/BoardErrors.purs
+++ src/Rymden/Data/BoardErrors.purs
@@ -2,7 +2,9 @@ module Rymden.Data.BoardErrors where
 
 import Rymden.Data.BorderSegment (BorderSegment)
 import Data.Set (Set)
+import Rymden.Data.Position (Position)
 
 type BoardErrors
   = { danglingBorders :: Set BorderSegment
+    , incorrectGalaxySizes :: Set Position
     }
--- src/Rymden/Helper/Foldable.purs
+++ src/Rymden/Helper/Foldable.purs
@@ -1,13 +1,29 @@
 module Rymden.Helper.Foldable where
 
 import Prelude
-
 import Data.Foldable (class Foldable, foldl)
 import Data.Map (Map)
 import Data.Map as Map
 import Data.Tuple (Tuple)
 
 count :: forall f a. Foldable f => Ord a => f a -> Map a Int
-count foldable =
-  foldl (\ map a -> Map.insertWith (+) a 1 map) Map.empty foldable
+count foldable = foldl (\map a -> Map.insertWith (+) a 1 map) Map.empty foldable
 
+mapmap ::
+  forall f g a b.
+  Functor f =>
+  Functor g =>
+  (a -> b) -> f (g a) -> f (g b)
+mapmap = (<$>) <<< (<$>)
+
+infix 2 mapmap as <$$>
+
+mapmapmap ::
+  forall f g h a b.
+  Functor f =>
+  Functor g =>
+  Functor h =>
+  (a -> b) -> f (g (h a)) -> f (g (h b))
+mapmapmap = (<$$>) <<< (<$>)
+
+infix 2 mapmapmap as <$$$>
