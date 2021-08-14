47b8754feb4d326dbf67307156ad0abfda26232a
Simon Olander <simon.olander@r2m.se>
2021-08-14 20:09:09 +0200

    Remove store from board component
    There's a bug where the store interacts in an unwanted way with child components.
    See https://github.com/purescript-halogen/purescript-halogen/issues/766

--- src/Rymden/Page/Home.purs
+++ src/Rymden/Page/Home.purs
@@ -1,48 +1,73 @@
 module Rymden.Page.Home where
 
 import Prelude
+import Data.Const (Const)
 import Effect.Class (class MonadEffect)
 import Halogen as H
 import Halogen.HTML as HH
-import Data.Const (Const)
-import Type.Proxy (Proxy(..))
-import Rymden.Component.Board as Board
+import Halogen.Store.Connect (Connected, connect)
 import Halogen.Store.Monad (class MonadStore)
+import Halogen.Store.Select (selectEq)
+import Rymden.Component.Board as Board
 import Rymden.Data.Store (Store)
-import Debug (spy)
+import Rymden.Data.WindowProperties (WindowProperties)
+import Type.Proxy (Proxy(..))
+import Data.Int (toNumber)
 
 type State
   = { solved :: Boolean
+    , window :: WindowProperties
     }
 
 data Action
   = ReceiveBoardOutput Board.Output
+
+type Input
+  = Unit
+
+type StoreInput
+  = Connected WindowProperties Input
 
 type Slots
   = ( board :: H.Slot (Const Void) Board.Output Unit
     )
 
 component ::
-  forall query input output storeAction m.
+  forall query output storeAction m.
   MonadEffect m =>
   MonadStore storeAction Store m =>
-  H.Component query input output m
-component = H.mkComponent { initialState, render, eval }
+  H.Component query Input output m
+component = connect (selectEq _.window) $ H.mkComponent { initialState, render, eval }
   where
-  initialState :: input -> State
-  initialState = const { solved: false }
+  initialState :: StoreInput -> State
+  initialState { context } =
+    { solved: false
+    , window: context
+    }
 
   render :: State -> HH.HTML (H.ComponentSlot Slots m Action) Action
-  render _state =
+  render state =
     HH.div
       []
-      [ HH.slot (Proxy :: _ "board") unit Board.component unit ReceiveBoardOutput
+      [ HH.slot (Proxy :: _ "board") unit Board.component boardInput ReceiveBoardOutput
       , HH.div_
           [ HH.button [] [ HH.text "Verify" ]
           ]
       ]
+    where
+    boardMaxWidth :: Number
+    boardMaxWidth = toNumber state.window.width
 
-  eval :: H.HalogenQ query Action input ~> H.HalogenM State Action Slots output m
+    boardMaxHeight :: Number
+    boardMaxHeight = toNumber state.window.height
+
+    boardInput :: Board.Input
+    boardInput =
+      { maxWidth: boardMaxWidth
+      , maxHeight: boardMaxHeight
+      }
+
+  eval :: H.HalogenQ query Action StoreInput ~> H.HalogenM State Action Slots output m
   eval =
     H.mkEval
       $ H.defaultEval
@@ -50,4 +75,4 @@ component = H.mkComponent { initialState, render, eval }
     where
     handleAction :: Action -> H.HalogenM State Action Slots output m Unit
     handleAction = case _ of
-      ReceiveBoardOutput (Board.Solved solved) -> H.modify_ _ { solved = spy "solved" solved }
+      ReceiveBoardOutput (Board.Solved solved) -> H.modify_ _ { solved = solved }
